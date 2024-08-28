06a8e8f549dc2f5fd0d4322b07cbbb59e6e43ea7
marothon <marothon@gmail.com>
2024-08-28 10:57:11 +0200

    Add flashing digits when the timer runs out

--- src/components/digital-display.tsx
+++ src/components/digital-display.tsx
@@ -5,8 +5,10 @@ export default function DigitalDisplay({totalSeconds}: {totalSeconds: number}) {
   const seconds = totalSeconds % 60;
   const formattedTime = `${minutes > 9 ? minutes : `0${minutes}`}:${seconds > 9 ? seconds : `0${seconds}`}`;
   
+  let className = "digital-display" + (totalSeconds <= 0 ? ' at-zero' : '');
+
   return (
-    <section className="digital-display">
+    <section className={className}>
       {formattedTime.split('').map((d, i) => 
         <span key={i}>{d}</span>
       )}
