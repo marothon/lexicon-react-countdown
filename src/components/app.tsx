a3db4739a0bb5d2fdc9794a3974d96c2369005ca
marothon <marothon@gmail.com>
2024-08-28 09:45:12 +0200

    Add interface for countdown to properly handle key

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,14 +1,25 @@
-import { FormEventHandler, useState } from 'react'
+import { FormEventHandler, useRef, useState } from 'react'
 import './App.css'
 import CountdownTimer from './countdown-timer'
 
+interface ICountdownTimer{
+  id: number,
+  timerStart: number
+}
+
 function App() {
-  const [countdownTimers, setCountdownTimers] = useState([60]);
+  const [countdownTimers, setCountdownTimers] = useState<Array<ICountdownTimer>>([{id: 1, timerStart: 60}]);
+  const identityRef = useRef<number>(1);
 
   const createNewTimer: FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault();
     const form = e.target as HTMLFormElement;
-    setCountdownTimers((ct) => [...ct, parseInt(form.timerStart.value)]);
+    const newCountDownTimer: ICountdownTimer = 
+      {
+        id: ++identityRef.current,
+        timerStart: parseInt(form.timerStart.value)
+      };
+    setCountdownTimers((ct) => [...ct, newCountDownTimer]);
     form.timerStart.value = '';
   }
 
@@ -17,7 +28,7 @@ function App() {
       <form className="new-countdown-form" onSubmit={createNewTimer}>
         <input name="timerStart" type="number" placeholder='Enter duration (s)'/>
       </form>
-      {countdownTimers.map((t) => <CountdownTimer timerStart={t}/>)}
+      {countdownTimers.map((t) => <CountdownTimer key={t.id} timerStart={t.timerStart}/>)}
     </>
   )
 }
