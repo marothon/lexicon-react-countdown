09a5526479ec86c431c809da26a28ceee6969af4
marothon <marothon@gmail.com>
2024-08-28 13:45:51 +0200

    Move input duration to CountdownTimer and remove feature to create new countdowns

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,35 +1,9 @@
-import { FormEventHandler, useRef, useState } from 'react'
 import './App.css'
 import CountdownTimer from './countdown-timer'
 
-interface ICountdownTimer{
-  id: number,
-  timerStart: number
-}
-
 function App() {
-  const [countdownTimers, setCountdownTimers] = useState<Array<ICountdownTimer>>([{id: 1, timerStart: 60}]);
-  const identityRef = useRef<number>(1);
-
-  const createNewTimer: FormEventHandler<HTMLFormElement> = (e) => {
-    e.preventDefault();
-    const form = e.target as HTMLFormElement;
-    const newCountDownTimer: ICountdownTimer = 
-      {
-        id: ++identityRef.current,
-        timerStart: parseInt(form.timerStart.value)
-      };
-    setCountdownTimers((ct) => [...ct, newCountDownTimer]);
-    form.timerStart.value = '';
-  }
-
   return (
-    <>
-      <form className="new-countdown-form" onSubmit={createNewTimer}>
-        <input name="timerStart" type="number" placeholder='Enter duration (s)'/>
-      </form>
-      {countdownTimers.map((t) => <CountdownTimer key={t.id} timerStart={t.timerStart}/>)}
-    </>
+      <CountdownTimer/>
   )
 }
 
--- src/components/countdown-timer.tsx
+++ src/components/countdown-timer.tsx
@@ -1,11 +1,12 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef, FormEventHandler } from 'react';
 import './countdown-timer.css';
 import DigitalDisplay from './digital-display';
 import useSound from 'use-sound';
 import alarmSfx from '../assets/sounds/digital-alarm-clock.mp3';
 
-function CountdownTimer({timerStart}: {timerStart: number}) {
-    const [timeLeft, setTimeLeft] = useState(timerStart);
+function CountdownTimer() {
+    const duration = useRef<number>(60)
+    const [timeLeft, setTimeLeft] = useState(duration.current);
     const [isActive, setIsActive] = useState(false);
     const intervalRef = useRef<number>();
     const [play, {stop}] = useSound(alarmSfx);
@@ -26,24 +27,34 @@ function CountdownTimer({timerStart}: {timerStart: number}) {
                     if(t-1 == 0){
                         setIsActive(false);
                     }
-                        return t-1;
+                        
+                    return t-1;
                 });
             }, 1000)
         }
-    
-
     }, [isActive])
 
     const resetCountdown = ()=>{
-        setTimeLeft(timerStart);
+        setTimeLeft(duration.current);
         stopAlarmSound();
     }
 
+    const updateDuration: FormEventHandler<HTMLFormElement> = (e) => {
+        e.preventDefault();
+        const form = e.target as HTMLFormElement;
+        duration.current = parseInt(form.timerStart.value);
+        form.timerStart.value = '';
+        resetCountdown();
+      }
+
     return (
         <div className="countdown-container">
+             <form className="new-countdown-form" onSubmit={updateDuration}>
+                <input name="timerStart" type="number" placeholder='Enter duration (s)'/>
+            </form>
             <DigitalDisplay totalSeconds={timeLeft}/>
             <div className="button-container">
-                <button disabled={timeLeft<=0}className='material-symbols-outlined' onClick={()=>{setIsActive(a => !a)}}>
+                <button disabled={timeLeft<=0   }className='material-symbols-outlined' onClick={()=>{setIsActive(a => !a)}}>
                     {isActive ? 'pause' : 'play_arrow'}
                 </button>
                 <button className='material-symbols-outlined' onClick={resetCountdown}>
